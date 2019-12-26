b6e736033198cb98d59946d1ddaaacb0380c0b62
Austin Reifsteck <apreifsteck@gmail.com>
2019-12-25 23:44:04 -0500

    added controls to BB --not hooked up to state yet

--- burger_builder_v1.0/src/components/Burger/BuildControls/BuildControls.jsx
+++ burger_builder_v1.0/src/components/Burger/BuildControls/BuildControls.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import BuildControl from './BuildControl/BuildControl'
+
 import classes from './BuildControls.module.css'
 
+const controls = [
+    { label: 'Salad', type: 'Salad' },
+    { label: 'Meat', type: 'Meat' },
+    { label: 'Cheese', type: 'Cheese' },
+    { label: 'Bacon', type: 'Bacon' },
+]
+
 const buildControls = (props) => {
+
+    const jsxControls = controls.map((item, i) => {
+        return <BuildControl label={item.label} type={item.type} key={i} />
+    })
+
     return (
-        
+        <div className={classes.BuildControls}>
+            {jsxControls}
+        </div>
     );
 };
 export default buildControls;
--- burger_builder_v1.0/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger_builder_v1.0/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,15 @@ import React, { Component } from 'react';
 import Aux from '../../hocs/Aux'
 
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+    Base: 4,
+    Salad: .2,
+    Bacon: .5,
+    Cheese: .1,
+    Meat: 1
+}
 
 class BurgerBuilder extends Component {
 
@@ -12,7 +21,21 @@ class BurgerBuilder extends Component {
             Bacon: 0,
             Cheese: 0,
             Meat: 0,
-        }
+        },
+        totalPrice: INGREDIENT_PRICES.Base
+    }
+
+    addIngredientHandler = (type) => {
+        const newIngredientCount = this.state.ingredients[type] + 1;
+        const newIngredients = { ...this.state.ingredients };
+        newIngredients[type] = newIngredientCount;
+        const newCost = Object.keys(this.state.ingredients).map((key, i) => {
+            return this.state.ingredients[key] * INGREDIENT_PRICES[key];
+        }).reduce((acc, curVal) => {
+            return acc + curVal
+        }, INGREDIENT_PRICES.Base)
+
+        this.setState({ ingredients: newIngredients, totalPrice: newCost })
     }
 
     render() {
@@ -20,7 +43,9 @@ class BurgerBuilder extends Component {
             <Aux>
                 <div>Burger</div>
                 <Burger ingredients={this.state.ingredients} />
+                <div>${this.state.totalPrice}</div>
                 <div>Builder controls</div>
+                <BuildControls click={this.addIngredientHandler} />
             </Aux>
         );
 
